c507936ea809b79c655cb3ce9ed59cc5947c8edd
MahirPatel3130 <patelmahir042@gmail.com>
2025-08-27 15:59:31 -0700

    Data transformation stage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.datascience.pipeline.data_transformation_pipeline import DataTransformationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -19,6 +20,16 @@ try:
     data_validation.initiate_data_validation()
     logger.info(
         f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx=============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    data_transformation = DataTransformationPipeline()
+    data_transformation.initiate_data_transformation()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<")
 except Exception as e:
     logger.exception(e)
     raise e
