ea13d99dcc501dbee00491416ec6ab737c442679
MahirPatel3130 <patelmahir042@gmail.com>
2025-08-28 14:16:13 -0700

    Model Trainer Completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationPipeline
+from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -29,6 +30,16 @@ try:
     logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
     data_transformation = DataTransformationPipeline()
     data_transformation.initiate_data_transformation()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Trainer stage"
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    mode_trainer = ModelTrainerPipeline()
+    mode_trainer.initiate_model_trainer()
     logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<")
 except Exception as e:
     logger.exception(e)
--- src/datascience/pipeline/data_validation_pipeline.py
+++ src/datascience/pipeline/data_validation_pipeline.py
@@ -15,12 +15,13 @@ class DataValidationTrainingPipeline:
         data_validation = DataValidation(config=data_validation_config)
         data_validation.validate_all_columns()
 
-    if __name__ == "__main__":
-        try:
-            logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<")
-            obj = DataValidationTrainingPipeline()
-            obj.initiate_data_validation()
-            logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<")
-        except Exception as e:
-            logger.exception(e)
-            raise e
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.initiate_data_validation()
+        logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
